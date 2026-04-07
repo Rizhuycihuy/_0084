79b2d0b946128f5f7164c1ec34c29e9e8e6d522a
Muhammad Rizki <rizki003131@gmail.com>
2026-04-07 15:06:02 +0700

    Perubahan 2

--- Array.cpp
+++ Array.cpp
@@ -1,3 +1,48 @@
 #include <iostream> 
 using namespace std;
 
+int main () {
+	int dataku [5] = {10,20,30,40,50}
+	int datamu[5];
+	int x;
+	cout << "data ke - 3 : " << data ku[2]<< endl;
+	x = dataku [2]*3;
+	dataku[2] = 600;
+	cout << "data ke - 3 yang baru : " << dataku[2]<< endl;
+	
+cout << "Data Index ke - 0 : " ;
+cin >> datamu[0];
+cout << "Data Index ke-1:";
+cin >> datamu[1];
+cout << "Data Index ke-2:";
+cin >> datamu [2];
+cout << "Data Index ke-3:";
+cin >> datamu[3];
+cout << "Data Index ke-4:";
+cin >> datamu[4];
+
+cout << "Data ke 1:"<< datamu [0]<<endl;
+cout << "Data ke-2:"<< datamu [1]<<endl;
+cout << "Data ke-3:"<< datamu [2]<<endl;
+cout << "Data ke-4: "<< datamu[3]<<endl;
+cout << "Data ke-5: "<< datamu[4]<<endl;
+
+cout << endl;
+for (int i = 0; i <= 4; i++){
+	cout << "Data Index ke - "<< i <<":";
+	cin >> datamu[i];
+}
+
+cout<<endl;
+for (int i = 0; i <= 4; i++)
+{
+	cout << "Data ke "<< i+1<<":"<< datamu[i]<<endl;
+}
+
+}
+
+
+
+
+
+
